6dfc1851ea9856c88fc6f49e6f4a0364f0364262
rafaeldiogo <fael.diogo@hotmail.com>
2022-05-29 14:57:43 -0300

    html-css-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
-$('.slider-principal').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
+$(".slider-principal").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
 });
